2172ae45312a7774d81e76069e93db36c435b220
Ada <Ada@PROBOOK>
2025-04-13 13:50:54 +0700

    Шаблоны функций в отдельных файлах

--- Functions5_Homework29march/Copy.h
+++ Functions5_Homework29march/Copy.h
@@ -6,6 +6,9 @@
 
 //копирует первый массив во второй
 template<typename T>
-void CopyArr(T arr[ROWS][COLS], T arr2[ROWS][COLS], const int ROWS, const int COLS);
-
-
+void CopyArr(T arr[ROWS][COLS], T arr2[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+		for (int j = 0; j < COLS; j++)
+			arr2[i][j] = arr[i][j];
+}
